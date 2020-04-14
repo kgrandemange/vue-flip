d1dd97346159adf80499e10bfe30230cfe382c4b
Kevin Grandemange <kevin.grandemange@cooperl-ext.com>
2020-04-14 19:38:37 +0200

    chore(build): set extract css to false

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-flip/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-flip/' : '/',
+  css: { extract: false }
 }
