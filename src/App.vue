452834ce93a96090650886de6dcd3df55bc04196
Kevin Grandemange <kevin@kevingrandemange.net>
2020-05-13 11:27:35 +0200

    docs: add header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,88 @@
 <template>
-  <div id="app">
-    <h1>Simple Example (hover)</h1>
-    <vue-flip :active-hover="true" width="200px" height="50px">
-      <template v-slot:front class="front">
-        front
-      </template>
-      <template v-slot:back class="back">
-        back
-      </template>
-    </vue-flip>
-    <pre v-highlightjs><code class="html">
-      &lt;vue-flip :active-hover="true" width="200px" height="50px"&gt;
+  <div>
+    <header>
+      <h1>Vue Flip</h1>
+      <h2>A component to flip elements</h2>
+    </header>
+    <section>
+      <h1>Simple Example (hover)</h1>
+      <vue-flip :active-hover="true" width="200px" height="50px">
+        <template v-slot:front class="front">
+          front
+        </template>
+        <template v-slot:back class="back">
+          back
+        </template>
+      </vue-flip>
+      <pre v-highlightjs><code class="html">
+        &lt;vue-flip :active-hover="true" width="200px" height="50px"&gt;
+          &lt;template v-slot:front class="front"&gt;
+            front
+          &lt;/template&gt;
+          &lt;template v-slot:back class="back"&gt;
+            back
+          &lt;/template&gt;
+        &lt;/vue-flip&gt;
+      </code></pre>
+
+      <h1>Simple Example (click)</h1>
+      <vue-flip :active-click="true" width="200px" height="50px">
+        <template v-slot:front class="front">
+          front
+        </template>
+        <template v-slot:back class="back">
+          back
+        </template>
+      </vue-flip>
+      <pre v-highlightjs><code class="html">
+        &lt;vue-flip :active-click="true" width="200px" height="50px"&gt;
+          &lt;template v-slot:front class="front"&gt;
+            front
+          &lt;/template&gt;
+          &lt;template v-slot:back class="back"&gt;
+            back
+          &lt;/template&gt;
+        &lt;/vue-flip&gt;
+      </code></pre>
+
+      <hr/>
+
+      <h1>Example with transition property</h1>
+      <vue-flip :active-hover="true" width="200px" height="50px" transition="2s">
+        <template v-slot:front class="front">
+          front
+        </template>
+        <template v-slot:back class="back">
+          back
+        </template>
+      </vue-flip>
+      <pre v-highlightjs><code class="html">
+        &lt;vue-flip :active-hover="true" width="200px" height="50px" transition="2s"&gt;
+          &lt;template v-slot:front class="front"&gt;
+            front
+          &lt;/template&gt;
+          &lt;template v-slot:back class="back"&gt;
+            back
+          &lt;/template&gt;
+        &lt;/vue-flip&gt;
+      </code></pre>
+      <hr/>
+
+      <h1>Flip card programmatically</h1>
+      <vue-flip :active-hover="true" width="200px" height="50px" class="simple-test" v-model="model">
+        <template v-slot:front class="front">
+          front
+        </template>
+        <template v-slot:back class="back">
+          back
+        </template>
+      </vue-flip>
+      <div style="margin: 10px">
+        <span style="margin-inline-end: 10px">{{ model }}</span>
+        <button @click="model=!model">Click</button>
+      </div>
+      <pre v-highlightjs><code class="html">
+        &lt;vue-flip :active-hover="true" width="200px" height="50px" class="simple-test" v-model="model"&gt;
         &lt;template v-slot:front class="front"&gt;
           front
         &lt;/template&gt;
@@ -18,98 +90,32 @@
           back
         &lt;/template&gt;
       &lt;/vue-flip&gt;
-    </code></pre>
+        &lt;div style="margin: 10px"&gt;
+          &lt;span style="margin-inline-end: 10px"&gt;{{ model }}&lt;/span&gt;
+          &lt;button @click="model=!model"&gt;Click&lt;/button&gt;
+        &lt;/div&gt;
+        &lt;/code&gt;&lt;/pre&gt;
+        &lt;hr/&gt;
 
-    <h1>Simple Example (click)</h1>
-    <vue-flip :active-click="true" width="200px" height="50px">
-      <template v-slot:front class="front">
-        front
-      </template>
-      <template v-slot:back class="back">
-        back
-      </template>
-    </vue-flip>
-    <pre v-highlightjs><code class="html">
-      &lt;vue-flip :active-click="true" width="200px" height="50px"&gt;
-        &lt;template v-slot:front class="front"&gt;
-          front
-        &lt;/template&gt;
-        &lt;template v-slot:back class="back"&gt;
-          back
-        &lt;/template&gt;
-      &lt;/vue-flip&gt;
-    </code></pre>
+        &lt;h1&gt;Flip one time&lt;/h1&gt;
+        &lt;vue-flip v-model="model2" class="simple-test" width="20%" height="50px"&gt;
+          &lt;template v-slot:front class="front"&gt;
+            &lt;div style="display:flex; align-items: center; justify-content: center; height:100%; width:100%" @click="handleClick"&gt;disable me!&lt;/div&gt;
+          &lt;/template&gt;
+          &lt;template v-slot:back class="back"&gt;
+            back
+          &lt;/template&gt;
+        &lt;/vue-flip&gt;
+      </code></pre>
 
-    <hr/>
+      <hr />
 
-    <h1>Example with transition property</h1>
-    <vue-flip :active-hover="true" width="200px" height="50px" transition="2s">
-      <template v-slot:front class="front">
-        front
-      </template>
-      <template v-slot:back class="back">
-        back
-      </template>
-    </vue-flip>
-    <pre v-highlightjs><code class="html">
-      &lt;vue-flip :active-hover="true" width="200px" height="50px" transition="2s"&gt;
-        &lt;template v-slot:front class="front"&gt;
-          front
-        &lt;/template&gt;
-        &lt;template v-slot:back class="back"&gt;
-          back
-        &lt;/template&gt;
-      &lt;/vue-flip&gt;
-    </code></pre>
-    <hr/>
+      <pairs />
 
-    <h1>Flip card programmatically</h1>
-    <vue-flip :active-hover="true" width="200px" height="50px" class="simple-test" v-model="model">
-      <template v-slot:front class="front">
-        front
-      </template>
-      <template v-slot:back class="back">
-        back
-      </template>
-    </vue-flip>
-    <div style="margin: 10px">
-      <span style="margin-inline-end: 10px">{{ model }}</span>
-      <button @click="model=!model">Click</button>
-    </div>
-    <pre v-highlightjs><code class="html">
-      &lt;vue-flip :active-hover="true" width="200px" height="50px" class="simple-test" v-model="model"&gt;
-      &lt;template v-slot:front class="front"&gt;
-        front
-      &lt;/template&gt;
-      &lt;template v-slot:back class="back"&gt;
-        back
-      &lt;/template&gt;
-    &lt;/vue-flip&gt;
-      &lt;div style="margin: 10px"&gt;
-        &lt;span style="margin-inline-end: 10px"&gt;{{ model }}&lt;/span&gt;
-        &lt;button @click="model=!model"&gt;Click&lt;/button&gt;
-      &lt;/div&gt;
-      &lt;/code&gt;&lt;/pre&gt;
-      &lt;hr/&gt;
+      <hr />
 
-      &lt;h1&gt;Flip one time&lt;/h1&gt;
-      &lt;vue-flip v-model="model2" class="simple-test" width="20%" height="50px"&gt;
-        &lt;template v-slot:front class="front"&gt;
-          &lt;div style="display:flex; align-items: center; justify-content: center; height:100%; width:100%" @click="handleClick"&gt;disable me!&lt;/div&gt;
-        &lt;/template&gt;
-        &lt;template v-slot:back class="back"&gt;
-          back
-        &lt;/template&gt;
-      &lt;/vue-flip&gt;
-    </code></pre>
-
-    <hr />
-
-    <pairs />
-
-    <hr />
-
-    <flip-cards />
+      <flip-cards />
+    </section>
   </div>
 </template>
 
@@ -147,7 +153,30 @@
     font-family: 'Open Sans', sans-serif;
   }
 
-  h1 {
+  body {
+    margin: 0;
+  }
+
+  header {
+    margin: 0;
+    background: linear-gradient(to right, #673AB7, #FFC107);
+    height: 20vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  header h1, header h2 {
+    margin: 0;
+    color: white;
+  }
+
+  section {
+    margin: 1rem;
+  }
+
+  h1, h2 {
     font-weight: 300;
   }
 
