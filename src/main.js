b5aead19287a879ccad2d1bafc733f996b07e156
Kevin Grandemange <kevin@kevingrandemange.net>
2020-05-15 12:50:11 +0200

    fix: remove composition-api

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueCompositionApi from '@vue/composition-api'
 import VueHighlightJS from 'vue-highlightjs'
 
 import './assets/arta.css'
 
 Vue.config.productionTip = false
 
-Vue.use(VueCompositionApi)
 Vue.use(VueHighlightJS)
 
 new Vue({
