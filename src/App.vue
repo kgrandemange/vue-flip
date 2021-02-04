4d5acb629e6597b93c5501441ca6f147acc2f375
Kevin Grandemange <kevin@kevingrandemange.net>
2021-02-04 19:35:01 +0100

    docs: add horizontal example

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,8 @@
     <section>
       <h1>Simple Example (hover)</h1>
       <vue-flip active-hover width="200px" height="50px">
-        <template v-slot:front class="front">
-          front
-        </template>
-        <template v-slot:back class="back">
-          back
-        </template>
+        <template v-slot:front class="front"> front </template>
+        <template v-slot:back class="back"> back </template>
       </vue-flip>
       <pre v-prism>
         <code>
@@ -29,12 +25,8 @@
 
       <h1>Simple Example (click)</h1>
       <vue-flip :active-click="true" width="200px" height="50px">
-        <template v-slot:front class="front">
-          front
-        </template>
-        <template v-slot:back class="back">
-          back
-        </template>
+        <template v-slot:front class="front"> front </template>
+        <template v-slot:back class="back"> back </template>
       </vue-flip>
       <pre v-prism>
         <code>
@@ -50,13 +42,14 @@
       </pre>
 
       <h1>Simple Example (click & hover)</h1>
-      <vue-flip :active-click="true" :activeHover="true" width="200px" height="50px">
-        <template v-slot:front class="front">
-          front
-        </template>
-        <template v-slot:back class="back">
-          back
-        </template>
+      <vue-flip
+        :active-click="true"
+        :activeHover="true"
+        width="200px"
+        height="50px"
+      >
+        <template v-slot:front class="front"> front </template>
+        <template v-slot:back class="back"> back </template>
       </vue-flip>
       <pre v-prism>
         <code>
@@ -71,16 +64,41 @@
         </code>
       </pre>
 
-      <hr/>
+      <hr />
+
+      <h1>Horizontal Example</h1>
+      <vue-flip
+        :active-click="true"
+        :activeHover="true"
+        width="200px"
+        height="50px"
+        horizontal
+      >
+        <template v-slot:front class="front"> front </template>
+        <template v-slot:back class="back">
+          <span style="transform: rotate(180deg)"> back </span>
+        </template>
+      </vue-flip>
+      <pre v-prism>
+        <code>
+        <div active-click width="200px" height="50px" :horizontal="true">
+          <p class="front">
+            front
+          </p>
+          <p class="back">
+            <span>
+              back
+            </span>
+          </p>
+        </div>
+        </code>
+      </pre>
+      <hr />
 
       <h1>Example with transition property</h1>
       <vue-flip active-hover width="200px" height="50px" transition="2s">
-        <template v-slot:front class="front">
-          front
-        </template>
-        <template v-slot:back class="back">
-          back
-        </template>
+        <template v-slot:front class="front"> front </template>
+        <template v-slot:back class="back"> back </template>
       </vue-flip>
       <pre v-prism>
         <code>
@@ -94,20 +112,22 @@
         </div>
         </code>
       </pre>
-      <hr/>
+      <hr />
 
       <h1>Flip card programmatically</h1>
-      <vue-flip active-click width="200px" height="50px" class="simple-test" v-model="model">
-        <template v-slot:front class="front">
-          front
-        </template>
-        <template v-slot:back class="back">
-          back
-        </template>
+      <vue-flip
+        active-click
+        width="200px"
+        height="50px"
+        class="simple-test"
+        v-model="model"
+      >
+        <template v-slot:front class="front"> front </template>
+        <template v-slot:back class="back"> back </template>
       </vue-flip>
       <div style="margin: 10px">
         <span style="margin-inline-end: 10px">{{ model }}</span>
-        <button @click="model=!model">Click</button>
+        <button @click="model = !model">Click</button>
       </div>
       <pre v-prism>
         <code>
@@ -126,16 +146,24 @@
         </code>
       </pre>
 
-      <hr/>
+      <hr />
 
       <h1>Flip one time</h1>
       <vue-flip v-model="model2" class="simple-test" width="20%" height="50px">
         <template v-slot:front class="front">
-          <span style="display:flex; align-items: center; justify-content: center; height:100%; width:100%" @click="handleClick">disable me!</span>
+          <span
+            style="
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 100%;
+              width: 100%;
+            "
+            @click="handleClick"
+            >disable me!</span
+          >
         </template>
-        <template v-slot:back class="back">
-          back
-        </template>
+        <template v-slot:back class="back"> back </template>
       </vue-flip>
 
       <pre v-prism>
@@ -175,85 +203,87 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import vueFlip from './components/Flip.vue'
-import Pairs from './components/Pairs.vue'
-import FlipCards from './components/FlipCards.vue'
+import { defineComponent } from "vue";
+import vueFlip from "./components/Flip.vue";
+import Pairs from "./components/Pairs.vue";
+import FlipCards from "./components/FlipCards.vue";
 
 export default defineComponent({
-  name: 'app',
+  name: "app",
   components: {
     vueFlip,
     Pairs,
-    FlipCards
+    FlipCards,
   },
   data: () => {
     return {
       model: false,
       model2: false,
-      model3: false
-    }
+      model3: false,
+    };
   },
   methods: {
-    handleClick () {
-      this.model2 = true
-    }
-  }
-})
+    handleClick() {
+      this.model2 = true;
+    },
+  },
+});
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap");
 
-  :root {
-    font-family: 'Open Sans', sans-serif;
-  }
+:root {
+  font-family: "Open Sans", sans-serif;
+}
 
-  body {
-    margin: 0;
-    min-width: 350px;
-  }
+body {
+  margin: 0;
+  min-width: 350px;
+}
 
-  header {
-    margin: 0;
-    background: linear-gradient(to right, #673AB7, #FFC107);
-    height: 20vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+header {
+  margin: 0;
+  background: linear-gradient(to right, #673ab7, #ffc107);
+  height: 20vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
-  header h1, header h2 {
-    margin: 0;
-    color: white;
-  }
+header h1,
+header h2 {
+  margin: 0;
+  color: white;
+}
 
-  section {
-    margin: 1rem;
-  }
+section {
+  margin: 1rem;
+}
 
-  h1, h2 {
-    font-weight: 300;
-  }
+h1,
+h2 {
+  font-weight: 300;
+}
 
-  .front {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #673AB7;
-    color: white;
-    width: 100%;
-    height: 100%;
-  }
+.front {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #673ab7;
+  color: white;
+  width: 100%;
+  height: 100%;
+}
 
-  .back {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #FFC107;
-    color: white;
-    width: 100%;
-    height: 100%;
-  }
+.back {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffc107;
+  color: white;
+  width: 100%;
+  height: 100%;
+}
 </style>
